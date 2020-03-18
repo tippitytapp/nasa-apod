982c649d39673c991f68043ac3cae12574e5374c
tippitytapp <60143534+tippitytapp@users.noreply.github.com>
2020-03-18 19:03:45 -0400

    finished it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import Title from "./Page Standards/title"
+import GetPic from "./Page Standards/axios"
 import "./App.css";
 
-function App() {
+
+
+
+function App(props) {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun 🚀!  
       </p>
-    </div>
+      <Title />
+      <GetPic />
+      <h2>{props.title}</h2>
+      <img src={props.hdurl} alt="NASA APOD"</img>
+>
+  
+      </div>
   );
 }
 
---
+++ src/Page Standards/title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+function Title (){
+return (
+<h1>NASA - Astronomy Photo Of The Day</h1>
+);
+}
+
+export default Title;
